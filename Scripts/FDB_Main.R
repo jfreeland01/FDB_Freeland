aae6f541f99129a4da469a4620bbda4245b4c689
jfreeland01 <108364706+jfreeland01@users.noreply.github.com>
2025-10-27 21:50:28 -0700

    pls working

--- Scripts/FDB_Main.R
+++ Scripts/FDB_Main.R
@@ -6,6 +6,9 @@ library(doRNG)
 library(missForest)
 library(dplyr)
 library(mixOmics)
+library(stringr)
+library(ggplot2)
+library(ggrepel)
 
 #### Imputation: CRISPR (NA's in Data) ####
 
@@ -16,7 +19,8 @@ CRISPR <- read.delim(
   file = file.crispr,
   row.names = 1,
   stringsAsFactors = F,
-  sep = ","
+  sep = ",",
+  check.names = F
 )
 
 table(colSums(is.na(CRISPR)))
@@ -62,13 +66,13 @@ path.dm   <- "/Users/jack/Library/CloudStorage/Box-Box/WD_FDB_Freeland/DataSets/
 path.ctrp <- "/Users/jack/Library/CloudStorage/Box-Box/WD_FDB_Freeland/DataSets/CTRPv2/"
 
 ## load in cell line info from depamp
-models <- read.delim(paste0(path.dm,"Model.csv"), sep = ",", stringsAsFactors = F)
+models <- read.delim(paste0(path.dm,"Model.csv"), sep = ",", stringsAsFactors = F, check.names = F)
 
 ## load in CTRP Data
-ctrp.expt   <- read.delim(paste0(path.ctrp,"v20.meta.per_experiment.txt"), sep = "\t", stringsAsFactors = F)
-ctrp.cell   <- read.delim(paste0(path.ctrp,"v20.meta.per_cell_line.txt"), sep = "\t", stringsAsFactors = F)
-ctrp.inform <- read.delim(paste0(path.ctrp,"CTRPv2.0._INFORMER_SET.txt"), sep = "\t", stringsAsFactors = F)
-ctrp.curves <- read.delim(paste0(path.ctrp,"v20.data.curves_post_qc.txt"), sep = "\t", stringsAsFactors = F)
+ctrp.expt   <- read.delim(paste0(path.ctrp,"v20.meta.per_experiment.txt"), sep = "\t", stringsAsFactors = F, check.names = F)
+ctrp.cell   <- read.delim(paste0(path.ctrp,"v20.meta.per_cell_line.txt"), sep = "\t", stringsAsFactors = F, check.names = F)
+ctrp.inform <- read.delim(paste0(path.ctrp,"CTRPv2.0._INFORMER_SET.txt"), sep = "\t", stringsAsFactors = F, check.names = F)
+ctrp.curves <- read.delim(paste0(path.ctrp,"v20.data.curves_post_qc.txt"), sep = "\t", stringsAsFactors = F, check.names = F)
 
 # temp <- as.data.frame(table(ctrp.expt$master_ccl_id))
 
@@ -89,10 +93,6 @@ write.table(
   x = ctrp.curves,
   file = paste0(path.ctrp,"ctrp.curves.txt"),
   quote = F, col.names=T, row.names = F, sep = "\t")
-
-# ctrp.rsl3 <-  ctrp.curves[ctrp.curves$cpd_name == "1S,3R-RSL-3",]
-# table(table(ctrp.rsl3$DepMap_ID))
-# write.table(ctrp.rsl3, paste0(path.ctrp,"ctrp.rsl3.txt"), quote = F, col.names=T, row.names = F, sep = "\t")
 
 ## trim and average data frame
 ctrp.curves.abr <- ctrp.curves %>% 
@@ -128,7 +128,7 @@ write.table(
 
 ctrpv2 <- ctrpv2.ave.wide
 
-## create a culled versio - rename one entry with no DepMap_ID - keep only drugs with data for > 20% of the cell lines
+## create a culled version - rename one entry with no DepMap_ID - keep only drugs with data for > 20% of the cell lines
 ctrpv3 <-  ctrpv2
 
 ## rename one entry with no DepMap_ID
@@ -182,17 +182,15 @@ write.table(
   col.names = NA
   )
 
-##### PLSR: CRISPR & CTRP #####
+##### PLSR Run: CRISPR & CTRP #####
 
 ## set paths
 path.dm   <- "/Users/jack/Library/CloudStorage/Box-Box/WD_FDB_Freeland/DataSets/DepMap_25Q3/"
 path.ctrp <- "/Users/jack/Library/CloudStorage/Box-Box/WD_FDB_Freeland/DataSets/CTRPv2/"
 
 ## read in data and set row names
-CRISPR <- read.delim(file = paste0(path.dm, "CRISPRGeneEffect_MFImputed.txt"), sep = "\t", stringsAsFactors = F) %>%
-  tibble::column_to_rownames(var = "X")
-CTRP <- read.delim(file = paste0(path.ctrp, "ctrpv2.wide_culled80_MFImputed.txt"), sep = "\t", stringsAsFactors = F) %>%
-  tibble::column_to_rownames(var = "X")
+CRISPR <- read.delim(file = paste0(path.dm, "CRISPRGeneEffect_MFImputed.txt"), sep = "\t", stringsAsFactors = F, check.names = F, row.names = 1)
+CTRP <- read.delim(file = paste0(path.ctrp, "ctrpv2.wide_culled80_MFImputed.txt"), sep = "\t", stringsAsFactors = F, check.names = F, row.names = 1)
 
 ## filter for shared cell lines, make matrix (for mixomics), ensure numeric
 ids <- intersect(rownames(CRISPR), rownames(CTRP))
@@ -217,14 +215,21 @@ pls_fit <- mixOmics::pls(
   mode  = "regression"    # default
 )
 
-## extract from pls_fit object
 print(pls_fit$prop_expl_var$X)
+print(pls_fit$prop_expl_var$Y)
 
-x.variates <- data.frame(pls_fit$variates$X)
-y.variates <- data.frame(pls_fit$variates$Y)
+## extract from pls_fit object
+x.variates <- data.frame(pls_fit$variates$X) %>%
+  tibble::rownames_to_column(var = "Score")
+y.variates <- data.frame(pls_fit$variates$Y) %>%
+  tibble::rownames_to_column(var = "Score")
 
-x.loadings <- data.frame(pls_fit$loadings$X)
-y.loadings <- data.frame(pls_fit$loadings$Y)
+x.loadings <- data.frame(pls_fit$loadings$X) %>%
+  tibble::rownames_to_column(var = "Loading") %>%
+  dplyr::arrange(comp1)
+y.loadings <- data.frame(pls_fit$loadings$Y) %>%
+  tibble::rownames_to_column(var = "Loading") %>%
+  dplyr::arrange(comp1)
 
 dim(x.variates);dim(x.loadings)
 dim(y.variates);dim(y.loadings)
@@ -232,21 +237,187 @@ dim(y.variates);dim(y.loadings)
 x.exp_variance <- data.frame(pls_fit$prop_expl_var$X)
 y.exp_variance <- data.frame(pls_fit$prop_expl_var$Y)
 
-variates.X <- cbind(Score = rownames(pls_fit$variates$X), x.variates)
-variates.Y <- cbind(Score = rownames(pls_fit$variates$Y), y.variates)
+# rownames(x.exp_variance) = paste0("comp.",seq(1,nrow(x.exp_variance)))
+# rownames(y.exp_variance) = paste0("comp.",seq(1,nrow(y.exp_variance)))
 
-loadings.X <- cbind(Loading = rownames(pls_fit$loadings$X), x.loadings)
-loadings.Y <- cbind(Loading = rownames(pls_fit$loadings$Y), y.loadings)
+variates.X.Y = merge(x.variates,y.variates,by="Score",suffixes = c(".geneexp",".crispr"))
 
-rownames(x.exp_variance) = paste0("comp.",seq(1,nrow(x.exp_variance)))
+## save files
+path.pls <- "/Users/jack/Library/CloudStorage/Box-Box/WD_FDB_Freeland/DataSets/PLS/"
 
-loadings.X.Y = merge(loadings.X,loadings.Y,by="Loading",suffixes = c(".geneexp",".crispr"))
-variates.X.Y = merge(variates.X,variates.Y,by="Score",suffixes = c(".geneexp",".crispr"))
+write.table(
+  x = x.variates,
+  file = paste0(path.pls, "PLS_Mode.Regression_X.CRISPR_Y.CTRP_X.variates.txt"),
+  sep = "\t", quote = F, row.names = F)
 
-# save files
-file.gs.gs = "PLSR geneexp vs crispr.txt"
+write.table(
+  x = y.variates,
+  file = paste0(path.pls, "PLS_Mode.Regression_X.CRISPR_Y.CTRP_Y.variates.txt"),
+  sep = "\t", quote = F, row.names = F)
 
-write.table(as.data.frame(variates.X.Y), paste0(gsub(".txt", "", file.gs.gs),"_ncomp",ncomp,"_weight.type-",weight.type, "_PLSR_X.Y.variates.txt"), sep = "\t", row.names = F, quote = F)
-write.table(as.data.frame(loadings.X.Y), paste0(gsub(".txt", "", file.gs.gs),"_ncomp",ncomp,"_weight.type-",weight.type, "_PLSR_X.Y.loadings.txt"), sep = "\t", row.names = F, quote = F)
+write.table(
+  x = variates.X.Y,
+  file = paste0(path.pls, "PLS_Mode.Regression_X.CRISPR_Y.CTRP_X.Y.variates.txt"),
+  sep = "\t", quote = F, row.names = F)
 
-write.table(as.data.frame(x.exp_variance), paste0(gsub(".txt", "", file.gs.gs),"_weight.type-",weight.type, "_PLSR_Xpve.txt"), sep = "\t", row.names = T, quote = F)
+write.table(
+  x = x.loadings,
+  file = paste0(path.pls, "PLS_Mode.Regression_X.CRISPR_Y.CTRP_X.loadings.txt"),
+  sep = "\t", quote = F, row.names = F)
+
+write.table(
+  x = y.loadings,
+  file = paste0(path.pls, "PLS_Mode.Regression_X.CRISPR_Y.CTRP_Y.loadings.txt"),
+  sep = "\t", quote = F, row.names = F)
+
+write.table(
+  x = x.exp_variance,
+  file = paste0(path.pls, "PLS_Mode.Regression_X.CRISPR_Y.CTRP_X.expvar.txt"),
+  sep = "\t", quote = F, row.names = F)
+
+write.table(
+  x = y.exp_variance,
+  file = paste0(path.pls, "PLS_Mode.Regression_X.CRISPR_Y.CTRP_Y.expvar.txt"),
+  sep = "\t", quote = F, row.names = F)
+
+##### PLSR Plot: CRISPR & CTRP #####
+
+#### load in drug data
+path.ctrp <- "/Users/jack/Library/CloudStorage/Box-Box/WD_FDB_Freeland/DataSets/CTRPv2/"
+path.pls <- "/Users/jack/Library/CloudStorage/Box-Box/WD_FDB_Freeland/DataSets/PLS/"
+
+ctrp.inform <- read.delim(file = paste0(path.ctrp,"CTRPv2.0._INFORMER_SET.txt"), sep = "\t", stringsAsFactors = F, check.names = F)
+Y_loadings <- read.delim(file = paste0(path.pls, "PLS_Mode.Regression_X.CRISPR_Y.CTRP_Y.loadings.txt"), sep = "\t", stringsAsFactors = F, check.names = F)
+
+## Prep for plotting
+lookup_indices <- match(Y_loadings$Loading, ctrp.inform$cpd_name)
+Y_loadings$drug.target <- ctrp.inform$target_or_activity_of_compound[lookup_indices]
+
+## adding extra collumns
+Y_loadings <- Y_loadings %>%
+  dplyr::mutate(
+    group = NA,
+    group = case_when(
+      stringr::str_detect(Loading, "^(selumetinib|PD318088|RAF265|regorafenib|PLX\\-4720|dabrafenib|GDC\\-0879)$") ~ "01 BRAFi.MEKi",
+      str_detect(Loading, "^(erlotinib|afatinib|lapatinib|neratinib|canertinib|vandetanib|gefitinib)$") ~ "02 EGFRi.HER2i",
+      stringr::str_detect(Loading, "^(1S\\,3R\\-RSL\\-3|ML210|erastin|ML162)") ~ "03 ferropt",
+      stringr::str_detect(Loading, "^(nutlin\\-3|HBX\\-41108|KU\\-60019)$") ~ "04 MDM2i",
+      stringr::str_detect(Loading, "^oligomycin[\\ .]?A$") ~ "05 oligomycinA",
+      stringr::str_detect(Loading, "^dasatinib") ~ "06 SRC",
+
+      stringr::str_detect(drug.target %||% "", "BCL2") & !str_detect(Loading, ":") ~ "07 BCL2+i",
+      
+      TRUE ~ NA_character_
+    ),
+    
+    # group.atp5 only for oligomycin A
+    group.atp5 = NA,
+    group.atp5 = if_else(str_detect(Loading, "^oligomycin[\\ .]?A$"), "05 oligomycinA", NA_character_),
+    
+    # NA flags & labels
+    group.na = if_else(is.na(group), 1L, 0L),
+    group.atp5.na = if_else(is.na(group.atp5), 1L, 0L),
+    label.not.na = if_else(!is.na(group), Loading, NA_character_),
+    label.not.na.atp5 = if_else(!is.na(group.atp5), Loading, NA_character_),
+    
+    # mix flag (dual vs single drug) based on presence of ':'
+    mix.flag = if_else(str_detect(Loading, ":"), "dual drug", "single drug")
+  ) %>%
+  arrange(desc(group.na))
+
+## Target category bucketing
+detect <- function(x, pattern) str_detect(x %||% "", regex(pattern, ignore_case = TRUE))
+
+Y_loadings <- Y_loadings %>%
+  mutate(target.category = NA_character_) %>%
+  mutate(target.category = if_else(detect(drug.target, "DNA damage"), "DNA.damage", target.category)) %>%
+  mutate(target.category = if_else(detect(drug.target, "(micro|mi)rotubule"), "microtubule", target.category)) %>%  # handles typos
+  mutate(target.category = if_else(detect(drug.target, "polo\\-like kinase 1|\\bPLK1\\b"), "PLK1", target.category)) %>%
+  mutate(target.category = if_else(detect(drug.target, "polo\\-like kinase 2|\\bPLK2\\b"), "PLK2", target.category)) %>%
+  mutate(target.category = if_else(detect(drug.target, "aurora kinase"), "aurora", target.category)) %>%
+  mutate(target.category = if_else(detect(drug.target, "DNA methyltransferase"), "DNA meth", target.category)) %>%
+  mutate(target.category = if_else(detect(drug.target, "DNA replication"), "DNA rep", target.category)) %>%
+  mutate(target.category = if_else(detect(drug.target, "nicotinamide phosphoribosyltransferase|\\bNAMPT\\b"), "NAMPT", target.category)) %>%
+  mutate(target.category = if_else(detect(drug.target, "dihydrofolate reductase|\\bDHFR\\b"), "DHFR", target.category)) %>%
+  mutate(target.category = if_else(detect(drug.target, "BCL2"), "BCL2.", target.category)) %>%
+  mutate(target.category = if_else(detect(drug.target, "XXXX"), "XXXX", target.category)) %>%
+  mutate(target.category = if_else(detect(drug.target, "XXXX"), "XXXX", target.category))
+
+
+## add in % NA's column ( Y from above section)
+percent.nas <- as.data.frame(colMeans(is.na(Y)) * 100)
+names(percent.nas) <- "percent.nas"
+percent.nas <- percent.nas %>%
+  tibble::rownames_to_column(var = "Loading")
+
+Y_loadings <- Y_loadings %>%
+  left_join(percent.nas, by = "Loading")
+
+Y_loadings <- Y_loadings %>%
+  dplyr::select(Loading, group, percent.nas, contains("target"), contains("flag"), everything())
+
+
+#### load in CRISPR data
+gene.info.all <- read.delim(
+  file = "/Users/jack/Library/CloudStorage/Box-Box/WD_FDB_Freeland/DataSets/General/NCBI Gene Homo_sapiens.gene_info 20220719.txt", sep = "\t", stringsAsFactors = F, check.names = F)
+gene.info <- gene.info.all[gene.info.all$Symbol_from_nomenclature_authority !="-",]
+gene.info.abr <- gene.info %>% dplyr::select(Symbol, description)
+
+X_loadings <- read.delim(file = paste0(path.pls, "PLS_Mode.Regression_X.CRISPR_Y.CTRP_X.loadings.txt"), sep = "\t", stringsAsFactors = F, check.names = F) %>%
+  dplyr::mutate(Loading = sub("\\.\\..*$", "", Loading))
+
+X_loadings <- merge(X_loadings, gene.info.abr, by.x="Loading", by.y="Symbol", all.x=T)
+
+X_loadings <- X_loadings %>%
+  dplyr::mutate(
+    group = case_when(
+      stringr::str_detect(Loading, "^(BRAF|MITF|MAPK1|SOX9|SOX10|PEA15|DUSP4)")     ~ "01 BRAF sig",
+      stringr::str_detect(Loading, "^(EGFR|KLF5|STX4|GRHL2|PIK3CA|ERBB2)$")         ~ "02 EGFR sig",
+      stringr::str_detect(Loading, "^(GPX4|SEPSECS|PSTK|EEFSEO|SEPHS2|SECISBP2)$")  ~ "03 ferropt",
+      stringr::str_detect(Loading, "^MDM[24]$")                                     ~ "04 MDM2.MDM4",
+      stringr::str_detect(Loading, "^ATP5")                                         ~ "05 ATP5",
+      stringr::str_detect(Loading, "^(ABL|SRC|LCK|LYN)")                            ~ "06 dasa targets",
+      stringr::str_detect(Loading, "^(BCL2|BCL2L1|BCL2L2|MCL1)$")                   ~ "07 BCL2+",
+      stringr::str_detect(Loading, "^MYC(|N|L)")                                    ~ "08 MYC.",
+      stringr::str_detect(Loading, "^(GRB2|CRKL)")                                  ~ "09 SRC-related",
+      stringr::str_detect(Loading, "^TP53$")                                        ~ "10 TP53",
+      stringr::str_detect(Loading, "^MED12$")                                       ~ "11 MED12",
+      TRUE ~ NA_character_
+    ),
+    group.atp5 = if_else(str_detect(Loading, "^ATP5"), "05 ATP5", NA_character_)
+  )
+
+X_loadings <- X_loadings %>%
+  dplyr::select(Loading, description, group, everything()) %>%
+  dplyr::mutate(
+    group.na         = if_else(is.na(group), 1L, 0L),
+    group.atp5.na    = if_else(is.na(group.atp5), 1L, 0L),
+    label.not.na     = if_else(!is.na(group), Loading, NA_character_),
+    label.not.na.atp5= if_else(!is.na(group.atp5), Loading, NA_character_)
+  ) %>%
+  dplyr::arrange(desc(group.na))
+
+# variates.X.Y.c.c.plotting = merge(samples %>% dplyr::select(stripped_cell_line_name,lineage_subtype,OncotreeLineage),variates.X.Y.c.c,by.x="stripped_cell_line_name",by.y="Score")
+
+## plotting
+my_colors <- c("#F8766D","#DE8C00","#B79F00","#00BA38","#00BF7D","#00BFC4","#00B4F0","#619CFF"
+               ,"hotpink","purple","cyan")
+my_colors_main <- my_colors
+
+ggplot2::ggplot(Y_loadings, aes_string(x = "comp1", y = "comp2", color = "target.category", label = "target.category"))  + 
+  geom_point() + 
+  geom_text_repel() +
+  geom_vline(xintercept = 0, linetype = "dashed", color = "grey40", size = 0.5) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "grey40", size = 0.5) + 
+  scale_color_manual(values = my_colors, na.value = "grey80") +
+  theme_minimal()
+
+
+
+
+
+
+
+
+
+
